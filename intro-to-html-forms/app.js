9d0f42debbccf70d08e795afc027f28cb971ee3c
jt <jacobtham3@gmail.com>
2025-05-02 13:55:42 +0800

    afternoon halfway

---
+++ intro-to-html-forms/app.js
@@ -0,0 +1,8 @@
+/* https://nodejs.org/api/assert.html */
+import { strict as assert } from "node:assert"; // assert.strictEqual(x, y) x = function | v = intended outcome | output is true or false
+// let sumTwoNum = (x, y) => {
+//   return x + y;
+// };
+// // console.log(sumTwoNum(2, 4));
+// assert.strictEqual(sumTwoNum(2, 4), 10);
+/* Python Tutor https://nodejs.org/api/assert.html */
